a05cfb009a4713ce0a6546c7e540b5bd89d53380
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2022-02-03 20:08:53 -0300

    Content: estructura y estilos

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react"
+import ListItem from "./ListItem"
+
 const Content = () => {
+
+  const [adverts, setAdverts] = useState(null)
+
+  useEffect(() => {
+    setTimeout(async () =>{
+      const res = await fetch('https://dev.to/api/articles')
+      const data = await res.json()
+      console.log({data})
+      setAdverts(data)
+    }, 2000)
+  }, [])
+
   return (
-    <h1>Content</h1>
+    <div className="content">
+      <header>
+        <h1>Se encontraron 4 resultados</h1>
+        <nav>
+          <button className="btn">Últimos</button>
+          <button className="btn">Tendencias</button>
+          <button className="btn">Promocionales</button>
+        </nav>
+      </header>
+      <main>
+        {adverts?.map((advert)=>(
+          <ListItem key={advert.id} data={advert} />
+        ))}
+      </main>
+    </div>
   )
 }
 export default Content
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,6 +1,14 @@
-const ListItem = () => {
+const ListItem = ({data}) => {
   return (
-    <h1>List Item</h1>
+    <article className="list-item">
+      <header>
+        <img src={data.social_image} alt="article" />
+      </header>
+      <div>{data.user?.name}</div>
+      <h3>{data.title}</h3>
+      <div>{data.tag_list?.map(tag => <span key={tag}> #{tag} </span> )}</div>
+      <footer></footer>
+    </article>
   )
 }
 export default ListItem
