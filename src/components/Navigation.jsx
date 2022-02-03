fbe72b3527f24170d3bf28917a3441a68a9deb80
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2022-02-03 06:46:16 -0300

    Navigation: estructura y estilos

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,26 @@
+import { FaGuitar, FaRegBell } from 'react-icons/fa'
+
 const Navigation = () => {
   return (
-    <h1>Navigation</h1>
+    <header className="header">
+      <div className="header-container">
+        <a href="/" className="header-logo">
+          <FaGuitar size="2rem" />
+          Searchband
+        </a>
+
+        <div className="header-right">
+          <a href="/register">Publicá gratis</a>
+          <button>Ingresar</button>
+          <i>
+            <FaRegBell />
+          </i>
+          <span className="header-user">
+            <img src="https://picsum.photos/200" alt="profile" />
+          </span>
+        </div>
+      </div>
+    </header>
   )
 }
 export default Navigation
