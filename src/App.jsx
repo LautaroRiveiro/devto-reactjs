dcabb79282fa1accd50ea0e931c99099eaf6e44f
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2022-02-03 15:08:17 -0300

    refactor: pase el manejo del drawer al LeftSidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-import { useState } from 'react'
 import './App.scss'
 import Content from "./components/Content"
-import Drawer from './components/Drawer'
 import LeftSidebar from "./components/LeftSidebar"
 import Navigation from "./components/Navigation"
 import NavMenu from './components/NavMenu'
 import RightSidebar from "./components/RightSidebar"
 
 const App = () => {
-
-  const [showDrawer, setShowDrawer] = useState(false)
-  const toggle = () => {
-    setShowDrawer(!showDrawer)
-  }
-
   return (
     <>
       <Navigation />
@@ -24,11 +16,6 @@ const App = () => {
         <Content />
         <RightSidebar />
       </main>
-      {showDrawer && (
-        <Drawer close={toggle}>
-          <NavMenu />
-        </Drawer>
-      )}
     </>
   )
 }
--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -1,8 +1,31 @@
-const LeftSidebar = ({ children }) => {
+import Drawer from './Drawer'
+import NavMenu from './NavMenu'
+import useToggle from '../hooks/useToggle'
+import { FaFilter } from "react-icons/fa"
+
+const LeftSidebar = () => {
+
+  const [showDrawer, toggle] = useToggle()
+
   return (
-    <aside className="left-sidebar">
-      {children}
-    </aside>
+    <>
+      <aside className="left-sidebar">
+        <NavMenu />
+      </aside>
+
+      <div className="drawer-opener" onClick={toggle}>
+        <button className="btn">
+          Filtros
+          <i><FaFilter /></i>
+        </button>
+      </div>
+
+      {showDrawer && (
+        <Drawer close={toggle} title="Menu">
+          <NavMenu />
+        </Drawer>
+      )}
+    </>
   )
 }
 export default LeftSidebar
