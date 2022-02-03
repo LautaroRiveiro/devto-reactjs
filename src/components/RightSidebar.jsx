ba6af0fd72569980c562f926838cec74abab06c1
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2022-02-03 18:12:36 -0300

    RightSidebar: Ejemplo informacion lateral

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -3,15 +3,47 @@ import Preview from "./Preview";
 const RightSidebar = () => {
 
   // TODO: Remove
-  const current = {};
+  const current = null;
 
   return (
     <aside className="right-sidebar">
       {current ? (
         <Preview />
-      ): (
-        <h1>Info</h1>
-        )}
+      ) : (
+        <>
+          <div className="right-sidebar-info-1">
+            <p>
+              <img src="https://picsum.photos/300/300" alt="info-1 cover" />
+            </p>
+            <h2>
+              <a href="/">Hack the planet</a>
+            </h2>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid dolorum nihil obcaecati, odit mollitia corrupti rem. Iste, aspernatur atque amet accusantium eos, ullam minima inventore suscipit, aperiam cumque necessitatibus et.
+              <span><a href="/"> Join the navi</a></span>
+            </p>
+          </div>
+
+          <div className="right-sidebar-info-2">
+            <header>
+              <h3>Title</h3>
+              <a href="/">See all</a>
+            </header>
+            <ul>
+              {[1, 2, 3].map((i) => (
+                <li key={i}>
+                  <a href="/">Text</a>
+                  {i % 2 === 1
+                    ? <span>new</span>
+                    : <small>1 comment</small>
+                  }
+                </li>
+              ))
+              }
+            </ul>
+          </div>
+        </>
+      )}
     </aside>
   )
 }
