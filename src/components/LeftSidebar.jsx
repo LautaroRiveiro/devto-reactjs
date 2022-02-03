9940ae67f334d5eb74e8c8821e81ce2b1c81fd7c
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2022-02-03 09:18:15 -0300

    LeftSidebar: Estructura y estilos

--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -1,6 +1,77 @@
+import { useState } from 'react'
+import { FaFacebookSquare, FaHome, FaInstagramSquare, FaTwitterSquare } from 'react-icons/fa'
+
 const LeftSidebar = () => {
+
+  const [showMore, setShowMore] = useState(false)
+  const toggle = (e) => {
+    e.preventDefault()
+    setShowMore(!showMore)
+  }
+
   return (
-    <h1>LeftSidebar</h1>
+    <aside className="left-sidebar">
+      <nav>
+        <ul>
+          <li>
+            <a href="/">
+              <i><FaHome /></i>
+              Inicio
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <i><FaHome /></i>
+              Inicio
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <i><FaHome /></i>
+              Inicio
+            </a>
+          </li>
+
+          {!showMore && (
+            <li className="more">
+              <a href="" onClick={toggle}>
+                Mostrar más
+              </a>
+            </li>
+          )}
+
+          {showMore && (
+            <>
+              <li>
+                <a href="/">
+                  <i><FaHome /></i>
+                  Inicio
+                </a>
+              </li>
+              <li>
+                <a href="/">
+                  <i><FaHome /></i>
+                  Inicio
+                </a>
+              </li>
+              <li className="more">
+                <a href="" onClick={toggle}>
+                  Mostrar menos
+                </a>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+
+      {showMore && (
+        <div className="left-sidebar-social">
+          <a href="/instragram"><i><FaInstagramSquare /></i></a>
+          <a href="/facebook"><i><FaFacebookSquare /></i></a>
+          <a href="/twitter"><i><FaTwitterSquare /></i></a>
+        </div>
+      )}
+    </aside>
   )
 }
 export default LeftSidebar
