39775e89309a100a4958d70b52f048b823bf78ee
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2022-02-03 21:19:06 -0300

    Content: Adecuacion grid y list items

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,12 +1,11 @@
-const ListItem = ({data}) => {
+const ListItem = ({ data }) => {
   return (
-    <article className="list-item">
+    <article className="card list-item">
       <header>
-        <img src={data.social_image} alt="article" />
+        <h3>{data.title}</h3>
       </header>
       <div>{data.user?.name}</div>
-      <h3>{data.title}</h3>
-      <div>{data.tag_list?.map(tag => <span key={tag}> #{tag} </span> )}</div>
+      <div>{data.tag_list?.map(tag => <span key={tag}> #{tag} </span>)}</div>
       <footer></footer>
     </article>
   )
